9915823e15d096e2110df9227383740186b7f149
Sreekumar <msreekumar@in.imshealth.com>
2021-08-04 13:16:17 +0530

    megha commit

---
+++ iqvia/mean/day3/myjs-INCHNWX22712.js
@@ -0,0 +1,6 @@
+console.log('My script');
+document.getElementById("hid").innerHTML="Welcome to java script";
+document.getElementById("hid").style.color='blue';
+function changeMe(){
+    document.getElementById("hid").style.backgroundColor='tomato';
+}
